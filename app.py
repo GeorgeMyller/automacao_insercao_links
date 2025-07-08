681f633662b2a860520d5c8e485fe815a6a2f79b
GeorgeMyller <george.myller@gmail.com>
2025-07-08 14:42:33 +0100

    Updates automation script coordinates and flow
    Adjusts mouse click coordinates to align with the target application's interface changes.
    
    Adds a scrolling action after inserting a link to navigate the source list.
    
    Corrects project name in lock file.

--- app.py
+++ app.py
@@ -6,20 +6,19 @@ with open('links.txt') as f:
     links = [line.strip() for line in f if line.strip()]
 
 
-
 time.sleep(5)  # Tempo para você posicionar o mouse na tela certa
-
+# Itera sobre cada link e executa as ações necessárias
 for link in links:
     #mova o mouse e clique no botão "Adicionar"
-    pyautogui.click(x=-606, y=627)  # Ajuste as coordenadas do mouse para o botão "Adicionar"
+    pyautogui.click(x=-949, y=548)  # Ajuste as coordenadas do mouse para o botão "Adicionar"
     #espere 1 segundos
     time.sleep(1)
-    # Clique no botão "YouTube" 
-    pyautogui.click(x=-501, y=972) # Ajuste as coordenadas do mouse para o botão "YouTube"
+    # Clique no botão do tipo de fonte "YouTube", "Colar Texto" ou "Site"
+    pyautogui.click(x=-607, y=902) # Ajuste as coordenadas do mouse para o botão "YouTube"
     #espere 1 segundos
     time.sleep(1)
     #clique na caixa de texto
-    pyautogui.click(x=-619, y=720) # Ajuste as coordenadas do mouse para a caixa de texto
+    pyautogui.click(x=-780, y=785) # Ajuste as coordenadas do mouse para a caixa de texto
     #espere 1 segundos
     time.sleep(1)
     #escreva o link
@@ -27,20 +26,28 @@ for link in links:
     #espere 1 segundos
     time.sleep(1)
     #clique no botão "Inserir"
-    pyautogui.click(x=-227, y=1064) # Ajuste as coordenadas do mouse para o botão "Inserir"
+    pyautogui.click(x=-181, y=912) # Ajuste as coordenadas do mouse para o botão "Inserir"
+    #espere 1 segundos
+    time.sleep(1)
+    #mover para a area de fontes para rolar
+    pyautogui.moveTo(x=-766, y=559)  # Ajuste as coordenadas do mouse para a área de fontes
+    #clicar na area de fontes e rolar para cima
+    pyautogui.click(x=-766, y=559)  # Ajuste as coordenadas do mouse para a área de fontes
+    #espere 1 segundos
+    time.sleep(1)
+    # Rolar para cima
+    pyautogui.vscroll(90)  # Rolar para cima
     #espere 1 segundos
     time.sleep(1)
 
-time.sleep(5)  # Tempo para você posicionar o mouse na tela certa
+"""
 
-'''
 
 #Para identificar a posição do mouse, descomente a linha abaixo e execute o script
-# print("Posicione o mouse sobre o botão.
+print("Posicione o mouse sobre o botão.")
 time.sleep(5)  # Tempo para você posicionar o mouse na tela certa
 print(pyautogui.position())# Ajuste a posição do mouse para o botão "Adicionar"
 
-'''
-
+"""
 
 print("Processo concluído!")
